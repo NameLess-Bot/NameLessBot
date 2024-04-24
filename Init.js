44c150bacd5733d2f13c392b210fbec84b39ae97
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-04-24 11:34:10 +0200

    Add log req

--- Init.js
+++ Init.js
@@ -5,12 +5,15 @@ const { stat } = require("fs");
 const { LoadCommandsFromDirectory } = require("./Modules/LoadCommands");
 const { DB_GetUserPermissionsByID } = require('./Modules/Database');
 
+// Load commands from the Commands directory
 const Commands = LoadCommandsFromDirectory(process.cwd() + '\\' + "Commands")
 
+// Create a new Discord client instance
 const client = new Client({
     intents: Config.Intents
 });
 
+// When the client is ready, log a message to the console
 client.on("ready", () => {
     console.log(`[*] Logged in as ${client.user.username}`);
     const UsedREST = new REST({ version: '10' }).setToken(Config.Token);
@@ -22,6 +25,7 @@ client.on("ready", () => {
     )
 })
 
+// When an interaction (slash command, button click, etc.) is created
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
     if (interaction.user.bot) return;
@@ -32,6 +36,7 @@ client.on('interactionCreate', async interaction => {
     let BotPermissions = await DB_GetUserPermissionsByID(parseInt(interaction.user.id), parseInt(interaction.guildId))
 
     if (ChannelPermissions.includes("ViewChannel") && (BotPermissions >= Commands[interaction.commandName].Access || interaction.member.permissions.has(PermissionsBitField.Flags.Administrator))) {
+        console.log(`[Command Executed] user ${interaction.user.username} executed command: ${interaction.commandName}`); // Console log de la commande exécutée
         Commands[interaction.commandName].Code(interaction)
     } else {
         const ErrorEmbed = new EmbedBuilder()
@@ -41,8 +46,10 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
+// When the bot joins a new guild, log a message to the console
 client.on("guildCreate", guild => {
     console.log(`[+] Joined : ${guild.name} (ID: ${guild.id})`);
 });
 
+// Login to Discord with the bot's token
 client.login(Config.Token)
